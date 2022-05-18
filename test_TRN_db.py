64eb1f571d100596c3d91348b665c14bb9f721f1
Denis Bulychev <denis_bulychev@epam.com>
2022-05-18 16:59:00 +0300

    v37

--- test_TRN_db.py
+++ test_TRN_db.py
@@ -10,9 +10,10 @@ def database_connection(driver=r'{ODBC Driver 17 for SQL Server}',
                 database='TRN'
                 ) -> pyodbc.Cursor:
     """ TRN database connection. """
-    #username = r'EPAM\Denis_Bulychev'
-    #password = ''
-    parameters = r'DRIVER={ODBC Driver 17 for SQL Server};SERVER=EPRUPETW0ECA\SQLEXPRESS;DATABASE=TRN;Trusted_Connection=yes;'
+    username = r'EPAM\Denis_Bulychev'
+    password = 'Belochka111'
+    parameters = r'DRIVER={ODBC Driver 17 for SQL Server};SERVER=EPRUPETW0ECA\SQLEXPRESS;DATABASE=TRN;Trusted_Connection=yes;' + \
+        ';UID=' + username + ';PWD=' + password
     cnxn = pyodbc.connect(parameters)
     return cnxn.cursor()
 
