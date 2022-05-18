8f1f5650fdce18a069b7616ba7f524aaba54ca84
Denis Bulychev <denis_bulychev@epam.com>
2022-05-18 22:25:14 +0300

    v40

--- test_TRN_db.py
+++ test_TRN_db.py
@@ -5,15 +5,10 @@ import pyodbc
 
 
 @pytest.fixture
-def database_connection(driver=r'{ODBC Driver 17 for SQL Server}',
-                server=r'EPRUPETW0ECA\SQLEXPRESS', 
-                database='TRN'
-                ) -> pyodbc.Cursor:
+def database_connection() -> pyodbc.Cursor:
     """ TRN database connection. """
-    username = r'Jenkins'
-    password = 'Belochka666'
-    parameters = r'DRIVER={SQL Server};SERVER=EPRUPETW0ECA\SQLEXPRESS;DATABASE=TRN;Trusted_Connection=no;' + \
-        ';UID=' + username + ';PWD=' + password
+    parameters = 'DRIVER={SQL Server};SERVER=EPRUPETW0ECA\SQLEXPRESS;' + \
+        'DATABASE=TRN;Trusted_Connection=no;UID=Jenkins;PWD=Belochka666'
     cnxn = pyodbc.connect(parameters)
     return cnxn.cursor()
 
